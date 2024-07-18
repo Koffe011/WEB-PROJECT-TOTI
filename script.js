12f876e18c231ff4bb7c2fd951547b564859b40c
Koffe011 <jmagr2002@gmail.com>
2024-07-18 00:32:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var nome = prompt("Olá! qual e sou nome?");
+var sobrenome = prompt("E, qual e sou sobrenome?");
+var edade = prompt("E sua edade?");
+
+alert ("sua reposta foi " + nome + sobrenome + edade );
+
+console.log(nome, sobrenome, edade,)
+
+console.table(nome, sobrenome, edade, {
+    nome,
+     sobrenome,
+      edade
+})
+
+console.table()
